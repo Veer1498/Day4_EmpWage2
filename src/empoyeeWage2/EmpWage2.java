656b6385eb000b4fdc8962f9008a7211cdc95a72
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-22 03:14:04 +0530

    Calculating wages for month

--- src/empoyeeWage2/EmpWage2.java
+++ src/empoyeeWage2/EmpWage2.java
@@ -4,6 +4,7 @@ public class EmpWage2 {
 	public static final int employeePresent = 1;
 	public static final int employeePartTime = 2;
 	public static final int WagePerHour = 20;
+	public static final int NumberOfWorkingDays =20;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stu
@@ -11,6 +12,8 @@ public class EmpWage2 {
 	
 		int numberOfHours = 0;
 		int empWagePerDay = 0;
+		int i, totalEmployeeWage =0;
+		for (i=0; i<NumberOfWorkingDays; i++) {
 		int isPresent = (int) Math.floor(Math.random()*10) %3;
 		switch (isPresent) {
 			case employeePresent :
@@ -24,11 +27,13 @@ public class EmpWage2 {
 			default :
 				numberOfHours = 0;
 				System.out.println("Employee is Absent");
-		
+				break;
 		}
 		empWagePerDay = numberOfHours * WagePerHour;
+		totalEmployeeWage= totalEmployeeWage + empWagePerDay;
 		System.out.println("Employee Wage daily =" + empWagePerDay);
-			
+		}	
+		System.out.println("Total Employee Wage is "+ totalEmployeeWage);
 	}
 
 }
