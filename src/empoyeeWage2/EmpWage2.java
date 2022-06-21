40d4a7ad11f0539d8b7e3838f593bc80eea9fb9d
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-22 02:26:53 +0530

    Calculating Daily Wage of employee

--- src/empoyeeWage2/EmpWage2.java
+++ src/empoyeeWage2/EmpWage2.java
@@ -6,11 +6,21 @@ public class EmpWage2 {
 		// TODO Auto-generated method stu
 		System.out.println("Welcome to Employee Wage Computation Program");
 		int employeePresent = 1;
+		int WagePerHour = 20;
+		int numberOfHours = 0;
+		int empWagePerDay = 0;
 		int isPresent = (int) Math.floor(Math.random()*10) %2;
-		if (isPresent == employeePresent)
+		if (isPresent == employeePresent) {
+			numberOfHours =8;
 			System.out.println("Employee is Present");
-		else
-			System.out.println("Employee is Absent");
+		}
+		else {
+			numberOfHours = 0;
+		System.out.println("Employee is Absent");
+		}
+		empWagePerDay = numberOfHours * WagePerHour;
+		System.out.println("Employee Wage daily =" + empWagePerDay);
+			
 	}
 
 }
