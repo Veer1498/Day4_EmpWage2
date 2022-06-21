65e129e1168736b0eec55dd5d16e12a3d9971eb5
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-22 01:18:38 +0530

    Checking Employee attendance

--- src/empoyeeWage2/EmpWage2.java
+++ src/empoyeeWage2/EmpWage2.java
@@ -5,6 +5,12 @@ public class EmpWage2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stu
 		System.out.println("Welcome to Employee Wage Computation Program");
+		int employeePresent = 1;
+		int isPresent = (int) Math.floor(Math.random()*10) %2;
+		if (isPresent == employeePresent)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 
 }
