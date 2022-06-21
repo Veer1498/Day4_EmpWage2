b48f38c5d5e98670674a8bb35366bc3daf11902c
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-22 01:03:55 +0530

    Welcome message

---
+++ src/empoyeeWage2/EmpWage2.java
@@ -0,0 +1,10 @@
+package empoyeeWage2;
+
+public class EmpWage2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		System.out.println("Welcome to Employee Wage Computation Program");
+	}
+
+}
